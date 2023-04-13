3830a05612fa8a99acaca8e0f0748dc238b7351d
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-04-13 12:23:46 +0200

    create layout.blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'fggfhfghfghfghfghdfghdfghfdghdfg'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'fggfhfghfghfghfghdfghdfghfdghdfg'
+            ]
+        ]
+    ]);
 });
