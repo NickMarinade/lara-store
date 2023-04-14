4f33148d5478757e29d83f993271f2576167bc8a
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-04-14 11:17:10 +0200

    make component for tags

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -11,28 +11,7 @@
             <h3 class="text-2xl">
                 <a href="{{ route('listing', ['listing' => $listing, 'slug' => $listing->slug]) }}">{{$listing->title}}</a>
             </h3>
-            <ul class="flex">
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Laravel</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">API</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Backend</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Vue</a>
-                </li>
-            </ul>
+            <x-listing-tags :tags="$listing->tags" />
         </div>
     </div>
 </x-card>
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -17,28 +17,9 @@
                         />
 
                         <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
-                        <ul class="flex">
-                            <li
-                                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                            >
-                                <a href="#">Laravel</a>
-                            </li>
-                            <li
-                                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                            >
-                                <a href="#">API</a>
-                            </li>
-                            <li
-                                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                            >
-                                <a href="#">Backend</a>
-                            </li>
-                            <li
-                                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                            >
-                                <a href="#">Vue</a>
-                            </li>
-                        </ul>
+
+                        <x-listing-tags :tags="$listing->tags" />
+                            
                         <div class="border border-gray-200 w-full mb-6"></div>
                         <div>
                             <h3 class="text-3xl font-bold mb-4">
