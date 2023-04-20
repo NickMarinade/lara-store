020774918c2410724abf1251ec82937605adacd3
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-04-20 16:22:55 +0200

    extra auth check for update/delete content

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -62,6 +62,10 @@ class ListingController extends Controller
 
     public function update(Request $request, Listing $listing) {
 
+        if($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
         $formFields = $request->validate([
             'title' => 'required',
             'tags' => 'required',
@@ -81,6 +85,11 @@ class ListingController extends Controller
     }
 
     public function destroy(Listing $listing) {
+
+        if($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+        
         $listing->delete();
         return redirect('/')->with('message', 'Content deleted!');
     }
